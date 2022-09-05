4e2f02079394cd28531017230db3b45b0bbef7b6
julia <julia.lereback@gmail.com>
2022-09-05 13:53:51 +0200

    Add cleanCode

--- src/Main.java
+++ src/Main.java
@@ -10,7 +10,7 @@ public class Main {
     public static void meny() {
         Scanner sc = new Scanner(System.in);
 
-        int[] price = new int[24];
+        int[] price = new int[5];
 
         String choice;
         do {
@@ -24,13 +24,13 @@ public class Main {
                     3. Sortera
                     4. Bästa Laddningstid (4h)
                     e. Avsluta
-                    
+                                        
                     """);
             choice = sc.nextLine();
             choice = choice.toLowerCase();
 
             switch (choice) {
-                case "1" -> price = input();
+                case "1" -> input(price);
                 case "2" -> minMaxAvg(price);
                 case "3" -> sort(price);
                 case "4" -> bestChargingTime(price);
@@ -40,38 +40,32 @@ public class Main {
         } while (!choice.equals("e"));
     }
 
-
-    public static int[] input() {
+    public static void input(int[] price) {
         Scanner sc = new Scanner(System.in);
 
         System.out.println("Skriv in priserna i hela ören och tryck på \"Enter\" mellan inmatningarna");
 
-        int[] price = new int[24];
-
-
         for (int i = 0; i < price.length; i++) {
             try {
+                System.out.println("Skriv in priset för klockan " + formatHour(i));
                 price[i] = Integer.parseInt(sc.nextLine());
             } catch (NumberFormatException e) {
                 System.out.println("Endast inmatning med heltal är accepterat");
-                input();
+                i--;
             }
-
         }
-
         System.out.println("Nu har du matat in alla priser");
-
-/*
-        HÄR VILL INTE PROGRAMMET GÅ TILLBAKA TILL STARTMENYN OM DEN HAR FÅNGAT NUMBERFORMATEXEPTION
-*/
-        return price;
     }
 
     private static void minMaxAvg(int[] price) {
-
-        //Lägsta priset
         int min = price[0];
         int minHour = 0;
+        getMinPrice(price, min, minHour);
+        getMaxPrice(price);
+        getAvgPrice(price);
+    }
+
+    private static void getMinPrice(int[] price, int min, int minHour) {
 
         for (int i = 0; i < price.length; i++) {
             if (price[i] < min) {
@@ -79,18 +73,14 @@ public class Main {
                 minHour = i;
             }
         }
+        printMinPrice(min, minHour);
+    }
 
-        if (minHour < 9)
-            System.out.println("Lägsta priset är " + min + " öre klockan 0" + minHour + "-0" + (minHour + 1));
-        else if (minHour == 9)
-            System.out.println("Lägsta priset är " + min + " öre klockan 0" + minHour + "-" + (minHour + 1));
-        else if (minHour >= 10 && minHour < 23)
-            System.out.println("Lägsta priset är " + min + " öre klockan " + minHour + "-" + (minHour + 1));
-        else
-            System.out.println("Lägsta priset är " + min + " öre klockan " + minHour + "-24");
+    private static void printMinPrice(int min, int minHour) {
+        System.out.println("Lägsta priset är " + min + " öre klockan " + formatHour(minHour));
+    }
 
-
-        //Högsta priset
+    private static void getMaxPrice(int[] price) {
         int max = price[0];
         int maxHour = 0;
 
@@ -100,84 +90,117 @@ public class Main {
                 maxHour = i;
             }
         }
+        printMaxPrice(max, maxHour);
+    }
 
-        if (maxHour < 9)
-            System.out.println("Högsta priset är " + max + " öre klockan 0" + maxHour + "-0" + (maxHour + 1));
-        else if (maxHour == 9)
-            System.out.println("Högsta priset är " + max + " öre klockan 0" + maxHour + "-" + (maxHour + 1));
-        else if (maxHour >= 10 && maxHour < 23)
-            System.out.println("Högsta priset är " + max + " öre klockan " + maxHour + "-" + (maxHour + 1));
-        else
-            System.out.println("Högsta priset är " + max + " öre klockan " + maxHour + "-24");
+    private static void printMaxPrice(int max, int maxHour) {
+        System.out.println("Högsta priset är " + max + " öre klockan " + formatHour(maxHour));
+    }
 
-
-        //Medelvärde
+    private static void getAvgPrice(int[] price) {
         int sum = 0;
 
         for (int i = 0; i < price.length; i++) {
             sum = sum + price[i];
         }
         double avg = (double) sum / price.length;
+        printAvgPrice(avg);
+    }
+
+    private static void printAvgPrice(double avg) {
         System.out.printf("Medelpriset för hela dygnet är %.2f öre\n", avg);
     }
 
     public static void sort(int[] price) {
         int[] priceClone = price.clone();
-        int[] index = new int[price.length];
+        int[] indexOfTime = new int[price.length];
 
         for (int i = 0; i < price.length; i++) {
-            index[i] = i;
+            indexOfTime[i] = i;
         }
 
         boolean bubbleSort = true;
-
         while (bubbleSort) {
             bubbleSort = false;
             for (int i = 0; i <= priceClone.length - 2; i++) {
                 if (priceClone[i] > priceClone[i + 1]) {
-                    int temp = priceClone[i + 1];
-                    priceClone[i + 1] = priceClone[i];
-                    priceClone[i] = temp;
-
-                    int indexTemp = index[i + 1];
-                    index[i + 1] = index[i];
-                    index[i] = indexTemp;
-
+                    sortWithBubble(priceClone, indexOfTime, i);
                     bubbleSort = true;
                 }
             }
         }
 
-        for (int i = 0; i < price.length; i++) {
-            if (index[i] < 9)
-                System.out.println("0" + index[i] + "-0" + (index[i] + 1) + "  ->  " + priceClone[i] + " öre");
-            else if (index[i] == 9)
-                System.out.println("0" + index[i] + "-" + (index[i] + 1) + "  ->  " + priceClone[i] + " öre");
-            else if (index[i] >= 10 && index[i] < 23)
-                System.out.println(index[i] + "-" + (index[i] + 1) + "  ->  " + priceClone[i] + " öre");
-            else
-                System.out.println(index[i] + "-24" + "  ->  " + priceClone[i] + " öre");
+        printSortedCode(price, priceClone, indexOfTime);
+    }
 
+    private static void sortWithBubble(int[] priceClone, int[] indexOfTime, int i) {
+        int temp = priceClone[i + 1];
+        priceClone[i + 1] = priceClone[i];
+        priceClone[i] = temp;
+
+        int indexTemp = indexOfTime[i + 1];
+        indexOfTime[i + 1] = indexOfTime[i];
+        indexOfTime[i] = indexTemp;
+    }
+
+    private static void printSortedCode(int[] price, int[] priceClone, int[] index) {
+        for (int i = 0; i < price.length; i++) {
+                System.out.println(formatHour(index[i]) + "  ->  " + priceClone[i] + " öre");
         }
     }
 
     public static void bestChargingTime(int[] price) {
         int lowPrice = Integer.MAX_VALUE;
-        int[] bestTimes = new int[2];
+        int[] bestHour = new int[2];
 
+        getBest4Hour(price, lowPrice, bestHour);
+        System.out.println("Det är bäst att ladda bilen från klockan " + formatRange(bestHour[0]) + " till " + formatRange(bestHour[1]));
+
+    }
+
+    private static void getBest4Hour(int[] price, int lowPrice, int[] bestHour) {
         for (int i = 0; i < price.length; i++) {
             if (i == price.length - 3)
                 break;
             int sum = price[i] + price[i + 1] + price[i + 2] + price[i + 3];
             if (sum < lowPrice) {
-                lowPrice = sum;
-                bestTimes[0] = i;
-                bestTimes[1] = i + 3;
-
+                lowPrice = setBest4Hour(bestHour, i, sum);
             }
         }
-        System.out.println("Det är bäst att ladda bilen mellan klockan " + bestTimes[0] + " och " + bestTimes[1]);
+    }
 
+    private static int setBest4Hour(int[] bestHour, int i, int sum) {
+        int lowPrice;
+        lowPrice = sum;
+        bestHour[0] = i;
+        bestHour[1] = i + 4;
+        return lowPrice;
+    }
+
+    public static String formatHour(int i) {
+        int hour = i + 1;
+        if (i < 9) {
+            return "0" + i + "-0" + hour;
+        } else if (i == 9) {
+            return "0" + i + "-" + hour;
+        } else if (i < 23) {
+            return i + "-" + hour;
+        } else {
+            return i + "-24";
+        }
+    }
+
+    private static String formatRange(int i) {
+        int hour = i + 1;
+        if (i < 9) {
+            return "0" + i;
+        } else if (i == 9) {
+            return "0" + i;
+        } else if (i < 23) {
+            return i + "";
+        } else {
+            return "24";
+        }
     }
 
     private static void quit() {
