aa431dd258fba93038c608bb3702ecfe2752bc31
Julia <julia.corell@gmail.com>
2022-09-10 20:41:56 +0200

    Add histogramInProgress

--- src/Main.java
+++ src/Main.java
@@ -264,152 +264,163 @@ public class Main {
     private static void histogramInProgress(int[] prices) {
 
 
-        int priceRow = 8;
-        int timeColumn = 27;
-        String[][] graphicMap = new String[priceRow][timeColumn];
-        addPrices(priceRow, timeColumn, prices, graphicMap);
-        printHistogram(priceRow, timeColumn, graphicMap);
-    }
+        int numRow = 8;
+        int numColumn = 27;
+        String[][] histogram = new String[numRow][numColumn];
 
-    private static void addPrices(int priceRow, int timeColumn, int[] prices, String[][] graphicMap) {
-        for (int i = 0; i < priceRow; i++) {
+        for (int h = 0; h < numRow; h++) {
             System.out.println();
-            for (int j = 0; j < timeColumn; j++) {
-                String maxNum = Integer.toString(getMaxPrice(prices));
-                String minNum = Integer.toString(getMinPrice(prices));
-                if (j == 0 && i == 0)
-                    System.out.print(maxNum);
-                else if (j == 0 && i == priceRow - 3) {
-                    for (j = 0; j < maxNum.length() - minNum.length(); j++) {
-                        System.out.print(" ");
-                    }
-                    System.out.print(minNum);
-                }
+            for (int w = 0; w < numColumn; w++) {
+                addMinMaxPrice(h, w, numRow, prices);
+                addAxis(h, w, numRow);
+                addHour(h, w, numRow);
+                addPrice(h, w, numRow);
             }
         }
     }
 
+    private static void addMinMaxPrice(int h, int w, int numRow, int[] prices) {
 
-            private static void printHistogram ( int priceRow, int timeColumn, String[][] graphicMap){
-                for (int i = 0; i < priceRow; i++) {
-                    for (int j = 0; j < timeColumn; j++) {
-                        System.out.print(graphicMap[i][j]);
-                    }
-                    System.out.println();
-                }
+        String maxNum = Integer.toString(getMaxPrice(prices));
+        String minNum = Integer.toString(getMinPrice(prices));
+        if (w == 0 && h == 0)
+            System.out.print(maxNum);
+        else if (w == 0 && h == numRow - 3) {
+            System.out.print(minNum);
+            for (w = 0; w < maxNum.length() - minNum.length(); w++) {
+                System.out.print(" ");
             }
+        } else if (w == 0) {
+            for (int i = 0; i < maxNum.length(); i++) {
+                System.out.print(" ");
+            }
+        }
+    }
+    private static void addAxis(int h, int w, int numRow) {
+        if (w == 1)
+            System.out.print("|");
+        else if (h == numRow - 2 && w > 1)
+            System.out.print("͟͟͟");
+    }
+    private static void addHour(int h, int w, int numRow) {
+        if (h == numRow - 1 && w > 1)
+            System.out.print(formatHourRange(w - 2) + " ");
+    }
+    private static void addPrice(int h, int w, int numRow) {
+
+    }
 
 
 //////////////////////////////////////////////
 
-            public static void graphics ( int[] prices){
-                String[][] map = new String[8][26];
-                createGraphBody(map, prices);
-                printVisualGraph(map);
+    public static void graphics(int[] prices) {
+        String[][] map = new String[8][26];
+        createGraphBody(map, prices);
+        printVisualGraph(map);
+    }
+
+    private static void createGraphBody(String[][] graph, int[] prices) {
+
+        int numSpace = getMaxPrice(prices);
+        int spaceHelp = getMinPrice(prices);
+
+
+        addHighestAndLowestPriceYAxis(graph, prices);
+        addYBorder(graph, numSpace, spaceHelp, prices);
+        addXBorder(graph);
+        addHoursToXAxis(graph);
+        addPriceRepresentationToGraph(graph, prices);
+    }
+
+    private static void addHighestAndLowestPriceYAxis(String[][] graph, int[] prices) {
+        graph[0][0] = String.valueOf(getMaxPrice(prices));
+        graph[6][0] = String.valueOf(getMinPrice(prices));
+    }
+
+    private static void addYBorder(String[][] graph, int numSpace, int spaceHelp, int[] prices) {
+        addVerticalWhiteSpaceAndNumberToGraph(graph, numSpace, spaceHelp, prices);
+        addPipeToVerticalBorder(graph);
+    }
+
+    private static void addPipeToVerticalBorder(String[][] graph) {
+        for (int i = 0; i < 8; i++) {
+            graph[i][1] = "|";
+        }
+    }
+
+    private static void addVerticalWhiteSpaceAndNumberToGraph(String[][] graph, int numSpace, int spaceHelp,
+                                                              int[] prices) {
+        for (int i = 1; i < 8; i++) {
+            graph[i][0] = printWhiteSpaces(numSpace);
+            if (i == 5)
+                graph[i][0] = printWhiteSpaces(numSpace - spaceHelp) + getMinPrice(prices);
+        }
+    }
+
+    private static String printWhiteSpaces(int numSpace) {
+        return " ".repeat(Math.max(0, numSpace));
+    }
+
+    private static void addXBorder(String[][] graph) {
+        for (int j = 2; j < 26; j++) {
+            graph[6][j] = "---";
+        }
+    }
+
+    private static void addHoursToXAxis(String[][] graph) {
+        int k = 2;
+        for (int i = 0; i < 24; i++) {
+            graph[7][k] = formatHourRange(i) + " ";
+            k++;
+        }
+    }
+
+    private static void addPriceRepresentationToGraph(String[][] graph, int[] prices) {
+        double priceCheck = (double) (getMaxPrice(prices)) / 6;
+        int divisionHelper = 6;
+
+        for (int i = 0; i < 6; i++) {
+            for (int j = 2; j < 26; j++) {
+                addPrice(graph, priceCheck, divisionHelper, i, j, prices);
+                addLastPrice(graph, i, j, prices);
             }
+            divisionHelper--;
+        }
+    }
 
-            private static void createGraphBody (String[][]graph,int[] prices){
+    private static void addPrice(String[][] graph, double priceCheck, int divisionHelper, int i, int j,
+                                 int[] prices) {
+        if (prices[j - 2] >= (priceCheck * divisionHelper)) {
+            graph[i][j] = " * ";
+        } else {
+            graph[i][j] = "   ";
+        }
+    }
 
-                int numSpace = getMaxPrice(prices);
-                int spaceHelp = getMinPrice(prices);
-
-
-                addHighestAndLowestPriceYAxis(graph, prices);
-                addYBorder(graph, numSpace, spaceHelp, prices);
-                addXBorder(graph);
-                addHoursToXAxis(graph);
-                addPriceRepresentationToGraph(graph, prices);
+    private static void addLastPrice(String[][] graph, int i, int j, int[] prices) {
+        if (i == 5) {
+            if (prices[j - 2] >= getMinPrice(prices)) {
+                graph[i][j] = " * ";
+            } else {
+                graph[i][j] = "   ";
             }
+        }
+    }
 
-            private static void addHighestAndLowestPriceYAxis (String[][]graph,int[] prices){
-                graph[0][0] = String.valueOf(getMaxPrice(prices));
-                graph[6][0] = String.valueOf(getMinPrice(prices));
+    private static void printVisualGraph(String[][] graph) {
+        for (int i = 0; i < 8; i++) {
+            for (int j = 0; j < 26; j++) {
+                System.out.print(graph[i][j]);
             }
-
-            private static void addYBorder (String[][]graph,int numSpace, int spaceHelp, int[] prices){
-                addVerticalWhiteSpaceAndNumberToGraph(graph, numSpace, spaceHelp, prices);
-                addPipeToVerticalBorder(graph);
-            }
-
-            private static void addPipeToVerticalBorder (String[][]graph){
-                for (int i = 0; i < 8; i++) {
-                    graph[i][1] = "|";
-                }
-            }
-
-            private static void addVerticalWhiteSpaceAndNumberToGraph (String[][]graph,int numSpace, int spaceHelp,
-            int[] prices){
-                for (int i = 1; i < 8; i++) {
-                    graph[i][0] = printWhiteSpaces(numSpace);
-                    if (i == 5)
-                        graph[i][0] = printWhiteSpaces(numSpace - spaceHelp) + getMinPrice(prices);
-                }
-            }
-
-            private static String printWhiteSpaces ( int numSpace){
-                return " ".repeat(Math.max(0, numSpace));
-            }
-
-            private static void addXBorder (String[][]graph){
-                for (int j = 2; j < 26; j++) {
-                    graph[6][j] = "---";
-                }
-            }
-
-            private static void addHoursToXAxis (String[][]graph){
-                int k = 2;
-                for (int i = 0; i < 24; i++) {
-                    graph[7][k] = formatHourRange(i) + " ";
-                    k++;
-                }
-            }
-
-            private static void addPriceRepresentationToGraph (String[][]graph,int[] prices){
-                double priceCheck = (double) (getMaxPrice(prices)) / 6;
-                int divisionHelper = 6;
-
-                for (int i = 0; i < 6; i++) {
-                    for (int j = 2; j < 26; j++) {
-                        addPrice(graph, priceCheck, divisionHelper, i, j, prices);
-                        addLastPrice(graph, i, j, prices);
-                    }
-                    divisionHelper--;
-                }
-            }
-
-            private static void addPrice (String[][]graph,double priceCheck, int divisionHelper, int i, int j,
-            int[] prices){
-                if (prices[j - 2] >= (priceCheck * divisionHelper)) {
-                    graph[i][j] = " * ";
-                } else {
-                    graph[i][j] = "   ";
-                }
-            }
-
-            private static void addLastPrice (String[][]graph,int i, int j, int[] prices){
-                if (i == 5) {
-                    if (prices[j - 2] >= getMinPrice(prices)) {
-                        graph[i][j] = " * ";
-                    } else {
-                        graph[i][j] = "   ";
-                    }
-                }
-            }
-
-            private static void printVisualGraph (String[][]graph){
-                for (int i = 0; i < 8; i++) {
-                    for (int j = 0; j < 26; j++) {
-                        System.out.print(graph[i][j]);
-                    }
-                    System.out.println("");
-                }
-            }
+            System.out.println("");
+        }
+    }
 
 
 /////////////////////////////////////////////////////
 
 
-            private static void quit () {
-                System.out.println("Programmet avslutas, välkommen åter");
-            }
-        }
+    private static void quit() {
+        System.out.println("Programmet avslutas, välkommen åter");
+    }
+}
