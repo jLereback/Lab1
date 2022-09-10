5c080b8a0e581af23b0adda694b2ff2dcef9d5d5
Julia <julia.corell@gmail.com>
2022-09-10 15:07:59 +0200

    Add histogramInProgress

--- src/Main.java
+++ src/Main.java
@@ -1,4 +1,3 @@
-import java.sql.SQLOutput;
 import java.util.Scanner;
 
 
@@ -10,8 +9,9 @@ public class Main {
 
     public static void menu() {
         Scanner sc = new Scanner(System.in);
+        System.out.println("Hur många timmar vill du använda?");
 
-        int[] prices = new int[5];
+        int[] prices = new int[sc.nextInt()];
 
         String choice;
         do {
@@ -24,7 +24,9 @@ public class Main {
                     2. Min, Max och Medel
                     3. Sortera
                     4. Bästa Laddningstid (4h)
-                    5. Horisontellt Histogram
+                    5. Horisontellt Histogram (graph)
+                    6. histogramInProgress
+                    7. graphics
                     e. Avsluta
                                         
                     """);
@@ -36,8 +38,9 @@ public class Main {
                 case "2" -> minMaxAvg(prices);
                 case "3" -> sort(prices);
                 case "4" -> bestChargingTime(prices);
-//                case "5" -> graph(prices);
-                case "5" -> graphInProgress(prices);
+                case "5" -> graph(prices);
+                case "6" -> histogramInProgress(prices);
+                case "7" -> graphics(prices);
                 case "e" -> quit();
                 default -> System.out.println("Vänligen välj ett av alternativen nedan:");
             }
@@ -56,7 +59,7 @@ public class Main {
     private static void inputPrice(int[] prices, Scanner sc) {
         for (int i = 0; i < prices.length; i++) {
             try {
-                System.out.println("Skriv in priset för klockan " + formatHour(i));
+                System.out.println("Skriv in priset för klockan " + formatHourOutput(i));
                 prices[i] = Integer.parseInt(sc.nextLine());
             } catch (NumberFormatException e) {
                 System.out.println("Endast inmatning med heltal är accepterat");
@@ -67,8 +70,8 @@ public class Main {
 
     private static void minMaxAvg(int[] prices) {
 
-        System.out.println("Lägsta priset är " + getMinPrice(prices) + " öre klockan " + formatHour(getMinHour(prices)));
-        System.out.println("Högsta priset är " + getMaxPrice(prices) + " öre klockan " + formatHour(getMaxHour(prices)));
+        System.out.println("Lägsta priset är " + getMinPrice(prices) + " öre klockan " + formatHourOutput(getMinHour(prices)));
+        System.out.println("Högsta priset är " + getMaxPrice(prices) + " öre klockan " + formatHourOutput(getMaxHour(prices)));
         System.out.printf("Medelpriset för hela dygnet är %.2f öre\n", getAvgPrice(prices));
     }
 
@@ -161,7 +164,7 @@ public class Main {
 
     private static void printSortedCode(int[] prices, int[] pricesClone, int[] index) {
         for (int i = 0; i < prices.length; i++) {
-            System.out.println(formatHour(index[i]) + "  ->  " + pricesClone[i] + " öre");
+            System.out.println(formatHourOutput(index[i]) + "  ->  " + pricesClone[i] + " öre");
         }
     }
 
@@ -170,7 +173,7 @@ public class Main {
         int[] bestHour = new int[2];
 
         getBest4Hour(prices, lowPrice, bestHour);
-        System.out.println("Det är bäst att ladda bilen från klockan " + formatRange(bestHour[0]) + " till " + formatRange(bestHour[1]));
+        System.out.println("Det är bäst att ladda bilen från klockan " + formatHourRange(bestHour[0]) + " till " + formatHourRange(bestHour[1]));
 
     }
 
@@ -200,55 +203,51 @@ public class Main {
         return indexOfTime;
     }
 
-    public static String formatHour(int i) {
-        int hour = i + 1;
-        if (i < 9) {
-            return "0" + i + "-0" + hour;
-        } else if (i == 9) {
-            return "0" + i + "-" + hour;
-        } else if (i < 23) {
-            return i + "-" + hour;
+    public static String formatHourOutput(int hour) {
+        int toNextHour = hour + 1;
+        if (hour < 9) {
+            return "0" + hour + "-0" + toNextHour;
+        } else if (hour == 9) {
+            return "0" + hour + "-" + toNextHour;
         } else {
-            return i + "-24";
+            return hour + "-" + toNextHour;
         }
     }
 
-    private static String formatRange(int i) {
-        if (i < 9) {
-            return "0" + i;
-        } else if (i == 9) {
-            return "0" + i;
-        } else if (i < 23) {
-            return i + "";
+    private static String formatHourRange(int hour) {
+        if (hour < 9) {
+            return "0" + hour;
+        } else if (hour == 9) {
+            return "0" + hour;
         } else {
-            return "24";
+            return String.valueOf(hour);
         }
     }
+
+
+////////////////////////////////////////
+
 
     private static void graph(int[] prices) {
-        System.out.println("Work in progress");
-    }
-
-    private static void graphInProgress(int[] prices) {
         int[] timeInHour = getTime(prices);
         String maxNum = Integer.toString(getMaxPrice(prices));
         String minNum = Integer.toString(getMinPrice(prices));
-        int numOfRows = 11;
-        for (int pRow = 0; pRow < numOfRows; pRow++) {
+        int numOfRows = 8;
+        for (int priceRow = 0; priceRow < numOfRows; priceRow++) {
             System.out.println();
-            for (int tColumn = 0; tColumn < timeInHour.length + 3; tColumn++) {
-                if (tColumn == 1)
+            for (int timeColumn = 0; timeColumn < timeInHour.length + 3; timeColumn++) {
+                if (timeColumn == 1)
                     System.out.print("|");
-                else if (pRow == numOfRows - 2 && tColumn > 1)
+                else if (priceRow == numOfRows - 2 && timeColumn > 1)
                     System.out.print("͟͟");
-                else if (tColumn == 0 && pRow == 0)
+                else if (timeColumn == 0 && priceRow == 0)
                     System.out.print(getMaxPrice(prices));
-                else if (tColumn == 0 && pRow == numOfRows - 3) {
+                else if (timeColumn == 0 && priceRow == numOfRows - 3) {
                     System.out.print(getMinPrice(prices));
                     for (int j = 0; j < maxNum.length() - minNum.length(); j++) {
                         System.out.print(" ");
                     }
-                } else if (tColumn == 0) {
+                } else if (timeColumn == 0) {
                     for (int i = 0; i < maxNum.length(); i++) {
                         System.out.print(" ");
                     }
@@ -258,7 +257,159 @@ public class Main {
         }
     }
 
-    private static void quit() {
-        System.out.println("Programmet avslutas, välkommen åter");
+
+////////////////////////////////////////
+
+
+    private static void histogramInProgress(int[] prices) {
+
+
+        int priceRow = 8;
+        int timeColumn = 27;
+        String[][] graphicMap = new String[priceRow][timeColumn];
+        addPrices(priceRow, timeColumn, prices, graphicMap);
+        printHistogram(priceRow, timeColumn, graphicMap);
     }
-}
+
+    private static void addPrices(int priceRow, int timeColumn, int[] prices, String[][] graphicMap) {
+        for (int i = 0; i < priceRow; i++) {
+            System.out.println();
+            for (int j = 0; j < timeColumn; j++) {
+                String maxNum = Integer.toString(getMaxPrice(prices));
+                String minNum = Integer.toString(getMinPrice(prices));
+                if (j == 0 && i == 0)
+                    System.out.print(maxNum);
+                else if (j == 0 && i == priceRow - 3) {
+                    for (j = 0; j < maxNum.length() - minNum.length(); j++) {
+                        System.out.print(" ");
+                    }
+                    System.out.print(minNum);
+                }
+            }
+        }
+    }
+
+
+            private static void printHistogram ( int priceRow, int timeColumn, String[][] graphicMap){
+                for (int i = 0; i < priceRow; i++) {
+                    for (int j = 0; j < timeColumn; j++) {
+                        System.out.print(graphicMap[i][j]);
+                    }
+                    System.out.println();
+                }
+            }
+
+
+//////////////////////////////////////////////
+
+            public static void graphics ( int[] prices){
+                String[][] map = new String[8][26];
+                createGraphBody(map, prices);
+                printVisualGraph(map);
+            }
+
+            private static void createGraphBody (String[][]graph,int[] prices){
+
+                int numSpace = getMaxPrice(prices);
+                int spaceHelp = getMinPrice(prices);
+
+
+                addHighestAndLowestPriceYAxis(graph, prices);
+                addYBorder(graph, numSpace, spaceHelp, prices);
+                addXBorder(graph);
+                addHoursToXAxis(graph);
+                addPriceRepresentationToGraph(graph, prices);
+            }
+
+            private static void addHighestAndLowestPriceYAxis (String[][]graph,int[] prices){
+                graph[0][0] = String.valueOf(getMaxPrice(prices));
+                graph[6][0] = String.valueOf(getMinPrice(prices));
+            }
+
+            private static void addYBorder (String[][]graph,int numSpace, int spaceHelp, int[] prices){
+                addVerticalWhiteSpaceAndNumberToGraph(graph, numSpace, spaceHelp, prices);
+                addPipeToVerticalBorder(graph);
+            }
+
+            private static void addPipeToVerticalBorder (String[][]graph){
+                for (int i = 0; i < 8; i++) {
+                    graph[i][1] = "|";
+                }
+            }
+
+            private static void addVerticalWhiteSpaceAndNumberToGraph (String[][]graph,int numSpace, int spaceHelp,
+            int[] prices){
+                for (int i = 1; i < 8; i++) {
+                    graph[i][0] = printWhiteSpaces(numSpace);
+                    if (i == 5)
+                        graph[i][0] = printWhiteSpaces(numSpace - spaceHelp) + getMinPrice(prices);
+                }
+            }
+
+            private static String printWhiteSpaces ( int numSpace){
+                return " ".repeat(Math.max(0, numSpace));
+            }
+
+            private static void addXBorder (String[][]graph){
+                for (int j = 2; j < 26; j++) {
+                    graph[6][j] = "---";
+                }
+            }
+
+            private static void addHoursToXAxis (String[][]graph){
+                int k = 2;
+                for (int i = 0; i < 24; i++) {
+                    graph[7][k] = formatHourRange(i) + " ";
+                    k++;
+                }
+            }
+
+            private static void addPriceRepresentationToGraph (String[][]graph,int[] prices){
+                double priceCheck = (double) (getMaxPrice(prices)) / 6;
+                int divisionHelper = 6;
+
+                for (int i = 0; i < 6; i++) {
+                    for (int j = 2; j < 26; j++) {
+                        addPrice(graph, priceCheck, divisionHelper, i, j, prices);
+                        addLastPrice(graph, i, j, prices);
+                    }
+                    divisionHelper--;
+                }
+            }
+
+            private static void addPrice (String[][]graph,double priceCheck, int divisionHelper, int i, int j,
+            int[] prices){
+                if (prices[j - 2] >= (priceCheck * divisionHelper)) {
+                    graph[i][j] = " * ";
+                } else {
+                    graph[i][j] = "   ";
+                }
+            }
+
+            private static void addLastPrice (String[][]graph,int i, int j, int[] prices){
+                if (i == 5) {
+                    if (prices[j - 2] >= getMinPrice(prices)) {
+                        graph[i][j] = " * ";
+                    } else {
+                        graph[i][j] = "   ";
+                    }
+                }
+            }
+
+            private static void printVisualGraph (String[][]graph){
+                for (int i = 0; i < 8; i++) {
+                    for (int j = 0; j < 26; j++) {
+                        System.out.print(graph[i][j]);
+                    }
+                    System.out.println("");
+                }
+            }
+
+
+/////////////////////////////////////////////////////
+
+
+            private static void quit () {
+                System.out.println("Programmet avslutas, välkommen åter");
+            }
+        }
